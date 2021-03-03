55a98629b0572d731974e2ab198a3a694fbb7b35
CyS <1440870444@qq.com>
2021-03-03 22:53:22 +0800

    2021.03.03 二分

---
+++ src/main/java/AcWing1_5.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/3/3
+ * 描述：整数二分
+ * 口诀：左段左移，右段右移，左移+1
+ */
+public class AcWing1_5 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line = input.readLine();
+        line = input.readLine();
+        int[] q = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+        while ((line = input.readLine()) != null) {
+            int x = Integer.parseInt(line);
+            int start = binarySearchStart(q, x);
+            int end = binarySearchEnd(q, x);
+            System.out.println(start + " " + end);
+        }
+    }
+
+    public static int binarySearchStart(int[] q, int x) {
+        int l = 0;
+        int r = q.length - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (q[mid] >= x) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return q[r] == x ? r : -1;
+    }
+
+    public static int binarySearchEnd(int[] q, int x) {
+        int l = 0;
+        int r = q.length - 1;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (q[mid] <= x) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return q[r] == x ? r : -1;
+    }
+}
---
+++ src/main/java/AcWing1_6.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/3/3
+ * 描述：浮点数二分求立方根
+ * 口诀：无边界问题，有精度问题
+ */
+public class AcWing1_6 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line = input.readLine();
+        double x = Double.parseDouble(line);
+        double cbrt = binarySearchFloat(x);
+        System.out.println(String.format("%.6f", cbrt));
+    }
+
+    private static double binarySearchFloat(double x) {
+        double l = -1 - Math.abs(x);
+        double r = 1 + Math.abs(x);
+        double eps = 1e-8;
+        while (r - l > eps) {
+            double mid = (l + r) / 2;
+            if (mid * mid * mid <= x) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+        return r;
+    }
+}
