7e86ebc41f0465d49870dd1f353554000d5e6a69
CyS <1440870444@qq.com>
2021-03-15 22:35:52 +0800

    2021.03.15 区间合并

--- src/main/java/AcWing1_20.java
+++ src/main/java/AcWing1_20.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author: CyS2020
@@ -13,6 +16,42 @@ public class AcWing1_20 {
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         String line = input.readLine();
+        Set<Pair> gaps = new TreeSet<>();
+        while ((line = input.readLine()) != null) {
+            int[] arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+            gaps.add(new Pair(arr[0], arr[1]));
+        }
+        int count = 0;
+        int ed = Integer.MIN_VALUE;
+        for (Pair pair : gaps) {
+            int left = pair.left;
+            int right = pair.right;
+            if (ed < left) {
+                count++;
+            }
+            ed = Math.max(ed, right);
+        }
+        System.out.println(count);
+    }
 
+    public static class Pair implements Comparable<Pair> {
+        int left;
+        int right;
+
+        public Pair(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public int compareTo(Pair pair) {
+            if (this.left != pair.left) {
+                return this.left < pair.left ? -1 : 1;
+            }
+            if (this.right != pair.right) {
+                return this.right < pair.right ? -1 : 1;
+            }
+            return 0;
+        }
     }
 }
