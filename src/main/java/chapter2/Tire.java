9c51e51d54f541a292ee9e0e8a024efc2003c0e3
CyS <1440870444@qq.com>
2021-03-28 20:44:18 +0800

    2021.03.28 Tire树

---
+++ src/main/java/chapter2/Tire.java
@@ -0,0 +1,70 @@
+package chapter2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/3/28
+ * 描述：全局边界不为null，开始即为son节点，循环结束即末尾
+ */
+
+class Tire {
+
+    public static Node root = new Node('0', 0);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line = input.readLine();
+        while ((line = input.readLine()) != null) {
+            String[] arr = line.split(" ");
+            String opera = arr[0];
+            String text = arr[1];
+            switch (opera) {
+                case "I":
+                    insert(text);
+                    break;
+                case "Q":
+                    int count = query(text);
+                    System.out.println(count);
+                    break;
+            }
+        }
+    }
+
+    public static void insert(String text) {
+        Node cur = root;
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (cur.next[ch - 'a'] == null) {
+                cur.next[ch - 'a'] = new Node(ch, 0);
+            }
+            cur = cur.next[ch - 'a'];
+        }
+        cur.count++;
+    }
+
+    public static int query(String text) {
+        Node cur = root;
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (cur.next[ch - 'a'] == null) {
+                return 0;
+            }
+            cur = cur.next[ch - 'a'];
+        }
+        return cur.count;
+    }
+
+    public static class Node {
+        char ch;
+        int count;
+        Node[] next = new Node[26];
+
+        public Node(char ch, int count) {
+            this.ch = ch;
+            this.count = count;
+        }
+    }
+}
