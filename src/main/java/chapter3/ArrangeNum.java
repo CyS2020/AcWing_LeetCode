e618a291139405cd102b8af9e909d2892df2e189
CyS <1440870444@qq.com>
2021-04-05 14:19:28 +0800

    2021.04.05 排序数字

---
+++ src/main/java/chapter3/ArrangeNum.java
@@ -0,0 +1,46 @@
+package chapter3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/4/4
+ * 描述：深度优先搜索
+ * 口诀：终止条件+恢复现场
+ */
+public class ArrangeNum {
+
+    public static int n;
+
+    public static int[] arr;
+
+    public static boolean[] st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line = input.readLine();
+        n = Integer.parseInt(line);
+        arr = new int[n + 1];
+        st = new boolean[n + 1];
+        dfs(1);
+    }
+
+    public static void dfs(int u) {
+        if (u > n) {
+            for (int i = 1; i <= n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!st[i]) {
+                arr[u] = i;
+                st[i] = true;
+                dfs(u + 1);
+                st[i] = false;
+            }
+        }
+    }
+}
