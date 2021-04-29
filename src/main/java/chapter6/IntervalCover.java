8ca41b992fef1c53dee2ca6e4196a797114b7a76
CyS <1440870444@qq.com>
2021-04-29 22:48:09 +0800

    2021.04.29 区间覆盖

---
+++ src/main/java/chapter6/IntervalCover.java
@@ -0,0 +1,70 @@
+package chapter6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/4/29
+ * 描述：区间覆盖
+ */
+public class IntervalCover {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line = input.readLine();
+        int[] arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int st = arr[0];
+        int ed = arr[1];
+        line = input.readLine();
+        int N = Integer.parseInt(line);
+        List<Pair> intervals = new ArrayList<>();
+        while ((line = input.readLine()) != null) {
+            arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+            intervals.add(new Pair(arr[0], arr[1]));
+        }
+
+        Collections.sort(intervals);
+
+        int cur = Integer.MIN_VALUE;
+        int res = 0;
+        int i;
+        while (cur < ed) {
+            for (i = 0; i < intervals.size(); i++) {
+                Pair pair = intervals.get(i);
+                if (pair.left <= st) {
+                    cur = Math.max(cur, pair.right);
+                } else {
+                    break;
+                }
+            }
+            if (cur == st) {
+                res = -1;
+                break;
+            }
+            st = cur;
+            res++;
+        }
+        System.out.println(res);
+    }
+
+    public static class Pair implements Comparable<Pair> {
+        int left;
+        int right;
+
+        public Pair(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public int compareTo(Pair pair) {
+            return this.left - pair.left;
+        }
+    }
+}
