4d89639614c25028a15b5edfe1b1768fd9309fde
CyS <1440870444@qq.com>
2021-04-29 22:48:18 +0800

    2021.04.29 区间合并

---
+++ src/main/java/chapter6/IntervalGroup.java
@@ -0,0 +1,60 @@
+package chapter6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/4/29
+ * 描述：区间分组
+ */
+public class IntervalGroup {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line = input.readLine();
+        int N = Integer.parseInt(line);
+        List<Pair> intervals = new ArrayList<>();
+        while ((line = input.readLine()) != null) {
+            int[] arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+            intervals.add(new Pair(arr[0], arr[1]));
+        }
+
+        Collections.sort(intervals);
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        int res = 0;
+        for (Pair pair : intervals) {
+            if (!queue.isEmpty() && queue.peek() < pair.left) {
+                queue.poll();
+                queue.add(pair.right);
+            } else {
+                queue.add(pair.right);
+                res++;
+            }
+        }
+        System.out.println(res);
+    }
+
+    public static class Pair implements Comparable<Pair> {
+        int left;
+        int right;
+
+        public Pair(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public int compareTo(Pair pair) {
+            return this.left - pair.left;
+        }
+    }
+}
