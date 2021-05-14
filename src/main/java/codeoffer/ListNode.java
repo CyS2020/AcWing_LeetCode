118887775d0f4a84cf94caac2d9e3cbc3ae7a79a
CyS <1440870444@qq.com>
2021-05-14 18:59:15 +0800

    2021.05.07 反转链表

---
+++ src/main/java/codeoffer/ListNode.java
@@ -0,0 +1,15 @@
+package codeoffer;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/5/13
+ * 描述：链表节点
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/codeoffer/ReverseList.java
@@ -0,0 +1,22 @@
+package codeoffer;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/5/14
+ * 描述：反转链表
+ * 思路：null <- 1 <- 2   3 -> 4 -> 5
+ * pre cur  next
+ */
+public class ReverseList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode next = null;
+        for (ListNode cur = head; cur != null; cur = next) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+        }
+        return pre;
+    }
+}
