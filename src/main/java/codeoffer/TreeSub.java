546a1b93bef926edc87a073691c7b0c1ac8d81f2
CyS <1440870444@qq.com>
2021-05-16 22:44:46 +0800

    2021.05.16 二叉树的子树

---
+++ src/main/java/codeoffer/TreeSub.java
@@ -0,0 +1,30 @@
+package codeoffer;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/5/16
+ * 描述：数的子结构
+ * 思路：两次dfs
+ */
+public class TreeSub {
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null) {
+            return false;
+        }
+        if (isSame(A, B)) {
+            return true;
+        }
+        return isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    public boolean isSame(TreeNode A, TreeNode B) {
+        if (B == null) {
+            return true;
+        }
+        if (A == null || A.val != B.val) {
+            return false;
+        }
+        return isSame(A.left, B.left) && isSame(A.right, B.right);
+    }
+}
