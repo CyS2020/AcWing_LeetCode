0f65e78de8cf3c278cfe5a4d8c9c0897017319ad
CyS <1440870444@qq.com>
2021-05-17 23:33:19 +0800

    2021.05.17 包含min函数的栈

---
+++ src/main/java/codeoffer/MinStack.java
@@ -0,0 +1,39 @@
+package codeoffer;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/5/17
+ * 描述：包含min函数的栈
+ * 思路：单调栈
+ */
+class MinStack {
+
+    Deque<Integer> stack = new LinkedList<>();
+
+    Deque<Integer> stackMin = new LinkedList<>();
+
+    public void push(int x) {
+        stack.addLast(x);
+        if (stackMin.isEmpty() || stackMin.peekLast() >= x) {
+            stackMin.addLast(x);
+        }
+    }
+
+    public void pop() {
+        int v = stack.pollLast();
+        if (v == stackMin.peekLast()) {
+            stackMin.pollLast();
+        }
+    }
+
+    public int top() {
+        return stack.peekLast();
+    }
+
+    public int min() {
+        return stackMin.peekLast();
+    }
+}
