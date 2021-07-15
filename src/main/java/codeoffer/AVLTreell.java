a3c0c445aef3ea761e507362d4fb13e4682f6327
CyS <1440870444@qq.com>
2021-07-15 23:24:42 +0800

    2021.07.15 平衡二叉树

---
+++ src/main/java/codeoffer/AVLTreell.java
@@ -0,0 +1,29 @@
+package codeoffer;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/7/15
+ * 描述：判断平衡二叉树
+ * 思路：递归求树的高度
+ */
+public class AVLTreell {
+
+    public boolean res = true;
+
+    public boolean isBalanced(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        if (Math.abs(left - right) > 1) {
+            res = false;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
