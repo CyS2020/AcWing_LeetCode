373254b78c30941fc1b39b854a2ea9209e7a3b15
CyS <1440870444@qq.com>
2021-07-17 12:02:52 +0800

    2021.07.17 二叉搜索树的公共祖先

---
+++ src/main/java/codeoffer/TreeAncestorl.java
@@ -0,0 +1,20 @@
+package codeoffer;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/7/16
+ * 描述：二叉搜索树的最近公共祖先
+ * 思路：二叉搜索树是有左小右大的顺序的
+ */
+public class TreeAncestorl {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
