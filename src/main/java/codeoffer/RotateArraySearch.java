efe538d931a1a7b765091fdebfded6479cfee092
CyS <1440870444@qq.com>
2021-08-22 22:47:34 +0800

    2021.08.22 搜索旋转排序数组

---
+++ src/main/java/codeoffer/RotateArraySearch.java
@@ -0,0 +1,35 @@
+package codeoffer;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/8/22
+ * 描述：搜索旋转排序数组
+ * 思路：二分查找
+ */
+public class RotateArraySearch {
+
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + r >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[nums.length - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
