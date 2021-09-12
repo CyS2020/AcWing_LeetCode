3401d1f602c669540ffb9746d1c06cabb5531054
CyS <1440870444@qq.com>
2021-09-12 14:54:10 +0800

    2021.09.12 重建二叉树

--- src/main/java/codeoffer/TreeBuild.java
+++ src/main/java/codeoffer/TreeBuild.java
@@ -12,11 +12,11 @@ import java.util.Map;
  * 口诀：前序遍历--根左右，中序遍历--左根右，后序遍历--左右根
  * 思路：距离等于 i - il + 1，位置等于 pl + i - il + 1 -1
  */
-public class BuildTree {
+public class TreeBuild {
 
-    Map<Integer, Integer> index = new HashMap<>();
+    private final Map<Integer, Integer> index = new HashMap<>();
 
-    int[] preorder;
+    private int[] preorder;
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         this.preorder = preorder;
@@ -26,16 +26,15 @@ public class BuildTree {
         return dfs(0, preorder.length - 1, 0, inorder.length - 1);
     }
 
-    private TreeNode dfs(int pl, int pr, int il, int ir) {
-        if (il > ir) {
+    private TreeNode dfs(int preLeft, int preRight, int inLeft, int inRight) {
+        if (inLeft > inRight) {
             return null;
         }
-        TreeNode root = new TreeNode(preorder[pl]);
-        int i = index.get(preorder[pl]);
-        TreeNode left = dfs(pl + 1, pl + i - il, il, i - 1);
-        TreeNode right = dfs(pl + i - il + 1, pr, i + 1, ir);
-        root.left = left;
-        root.right = right;
-        return root;
+        int rootVal = preorder[preLeft];
+        int rootLoc = index.get(preorder[preLeft]);
+        int leftLen = rootLoc - inLeft;
+        TreeNode leftNode = dfs(preLeft + 1, preLeft + leftLen, inLeft, rootLoc - 1);
+        TreeNode rightNode = dfs(preLeft + leftLen + 1, preRight, rootLoc + 1, inRight);
+        return new TreeNode(rootVal, leftNode, rightNode);
     }
 }
