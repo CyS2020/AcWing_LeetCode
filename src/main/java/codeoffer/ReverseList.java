b7f86130c9486396cc9fa9cc172273116e49e6f3
CyS <1440870444@qq.com>
2021-09-19 18:40:21 +0800

    2021.09.19 反转链表

--- src/main/java/codeoffer/ReverseList.java
+++ src/main/java/codeoffer/ReverseList.java
@@ -7,13 +7,13 @@ import base.ListNode;
  * @date: 2021/5/14
  * 描述：反转链表
  * 思路：null <- 1 <- 2   3 -> 4 -> 5
- * pre cur  next
+ * pre cur next
  */
 public class ReverseList {
 
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
-        ListNode next = null;
+        ListNode next;
         for (ListNode cur = head; cur != null; cur = next) {
             next = cur.next;
             cur.next = pre;
