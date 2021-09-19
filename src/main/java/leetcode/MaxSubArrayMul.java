e727ef5ad2464d462c3dfc443c7286e0fa290aa1
CyS <1440870444@qq.com>
2021-09-19 16:01:19 +0800

    2021.09.19 乘积最大子数组

---
+++ src/main/java/leetcode/MaxSubArrayMul.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/9/19
+ * 描述：乘积最大子数组
+ * 思路：遍历某个位置时, 该值要么乘上最大值, 乘上最大值, 或者不乘
+ */
+public class MaxSubArrayMul {
+
+    public int maxProduct(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        int res = max;
+        for (int i = 1; i < nums.length; i++) {
+            int tmp = max;
+            max = Math.max(Math.max(nums[i] * min, nums[i] * tmp), nums[i]);
+            min = Math.min(Math.min(nums[i] * min, nums[i] * tmp), nums[i]);
+            res = Math.max(res, max);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/leetcode/MaxSubArraySum.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/5/30
+ * 描述：最大子序和
+ * 思路：遍历某个位置时, 该值要么加上前面的和要么不加
+ */
+public class MaxSubArraySum {
+
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(nums[i] + sum, nums[i]);
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
