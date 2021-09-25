f6254eb214b2ad8fd1b530ba3f9980debf5172f0
CyS <1440870444@qq.com>
2021-09-25 22:11:29 +0800

    2021.09.25 搜索二维矩阵

--- src/main/java/codeoffer/FindIn2DArrayII.java
+++ src/main/java/codeoffer/FindIn2DArrayII.java
@@ -3,18 +3,19 @@ package codeoffer;
 /**
  * @author: CyS2020
  * @date: 2021/5/6
- * 描述：二维数组中的查找
+ * 描述：搜索二维矩阵
  * 思路：从右上角进行查找，大于目标值往左，小于目标值往下
  */
-public class FindIn2DArray {
+public class FindIn2DArrayII {
 
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
         int row = matrix.length;
+        int col = matrix[0].length;
         int i = 0;
-        int j = matrix[0].length - 1;
+        int j = col - 1;
         while (i < row && j >= 0) {
             if (matrix[i][j] == target) {
                 return true;
