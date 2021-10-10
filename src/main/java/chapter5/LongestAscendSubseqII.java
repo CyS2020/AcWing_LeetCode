96bdb0287ffbc82496e19bca96b8423cc38caefc
CyS <1440870444@qq.com>
2021-10-10 16:14:34 +0800

    2021.10.10 最长上升子序列

--- src/main/java/chapter5/LongestAscendSubseqII.java
+++ src/main/java/chapter5/LongestAscendSubseqII.java
@@ -9,33 +9,31 @@ import java.util.Arrays;
  * @author: CyS2020
  * @date: 2021/4/24
  * 描述：最长上升子序列II
- * 二分优化
+ * 思路：二分优化与桶排序
  */
 public class LongestAscendSubseqII {
+
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         String line = input.readLine();
         int N = Integer.parseInt(line);
-        int[] s = new int[N + 1];
-        int[] p = new int[N + 1];
         line = input.readLine();
         int[] arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
-        System.arraycopy(arr, 0, s, 1, N);
 
+        int[] p = new int[N + 1];
         int len = 0;
-        for (int i = 1; i <= N; i++) {
-            int l = lowerBound(s[i], p, len, i);
-
+        for (int i = 0; i < N; i++) {
+            int l = lowerBound(arr[i], p, len);
             if (l == len) {
                 len++;
             }
-            p[l + 1] = s[i];
+            p[l + 1] = arr[i];
         }
 
         System.out.println(len);
     }
 
-    private static int lowerBound(int t, int[] p, int len, int i) {
+    private static int lowerBound(int t, int[] p, int len) {
         int l = 0;
         int r = len;
         while (l < r) {
