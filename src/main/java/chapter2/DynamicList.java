59e7ce5dd453eb360432079c5eb1f814fb080f70
CyS2020 <34827011+CyS2020@users.noreply.github.com>
2023-08-22 13:46:31 +0800

    Update DynamicList.java

--- src/main/java/chapter2/DynamicList.java
+++ src/main/java/chapter2/DynamicList.java
@@ -8,13 +8,13 @@ import java.io.InputStreamReader;
  * @author: CyS2020
  * @date: 2021/3/16
  * 描述：动态单链表
- * 口诀：先找儿子后找爹，全局边界初为null，边界增删特处理，游标从0k-1
+ * 口诀：添加时先右后左，全局边界初为null，边界增删特处理，游标从0k-1
  */
 public class DynamicList {
 
     public static int index = 1;
 
-    public static Node head = null;
+    public static Node head = new Node(0, 0);
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -23,7 +23,7 @@ public class DynamicList {
             String[] arr = line.split(" ");
             switch (arr[0]) {
                 case "H":
-                    addToHead(Integer.parseInt(arr[1]));
+                    addToKth(0, Integer.parseInt(arr[1]));
                     index++;
                     break;
                 case "I":
@@ -35,16 +35,12 @@ public class DynamicList {
                     break;
             }
         }
-        for (Node cur = head; cur != null; cur = cur.next) {
+        for (Node cur = head.next; cur != null; cur = cur.next) {
             System.out.print(cur + " ");
         }
     }
 
     public static void deleteKth(int k) {
-        if (k == 0) {
-            head = head.next;
-            return;
-        }
         Node cur = head;
         while (cur.index != k) {
             cur = cur.next;
@@ -60,12 +56,6 @@ public class DynamicList {
         Node newNode = new Node(value, index);
         newNode.next = cur.next;
         cur.next = newNode;
-    }
-
-    public static void addToHead(int value) {
-        Node newNode = new Node(value, index);
-        newNode.next = head;
-        head = newNode;
     }
 
     public static class Node {
