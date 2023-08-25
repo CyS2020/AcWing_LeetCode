70b34cb0c48a09c0cddff9fcdac368ccf44b0821
CyS2020 <34827011+CyS2020@users.noreply.github.com>
2023-08-25 18:18:34 +0800

    Update BellmanFord.java

--- src/main/java/chapter3/BellmanFord.java
+++ src/main/java/chapter3/BellmanFord.java
@@ -1,12 +1,9 @@
 package chapter3;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author: CyS2020
@@ -16,59 +13,63 @@ import java.util.List;
  */
 public class BellmanFord {
 
-    public static int n;
+    public static int n, k;
+
+    public static Node[] heads;
 
     public static int[] dist;
-
-    public static List<Edge> edges;
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         String line = input.readLine();
         int[] arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
         n = arr[0];
-        int k = arr[2];
+        k = arr[2];
+        heads = new Node[n + 1];
         dist = new int[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE / 2);
-        edges = new ArrayList<>();
         while ((line = input.readLine()) != null) {
             arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
-            int a = arr[0];
-            int b = arr[1];
-            int w = arr[2];
-            edges.add(new Edge(a, b, w));
+            addEdge(arr[0], arr[1], arr[2]);
         }
 
-        int distance = calcShortestPath(1, n, k);
-        System.out.println(distance == -1 ? "impossible" : distance);
+        String distance = calcShortestPath(1, n);
+        System.out.println(distance);
     }
 
-    private static int calcShortestPath(int src, int dst, int k) {
+    private static String calcShortestPath(int src, int dst) {
         dist[src] = 0;
         int[] backup = new int[n + 1];
-
-        for (int i = 1; i <= k; i++) {
-            System.arraycopy(dist, 1, backup, 1, n);
-            for (Edge edge : edges) {
-                int a = edge.src;
-                int b = edge.dst;
-                int w = edge.weight;
-                if (backup[a] + w < dist[b]) {
-                    dist[b] = backup[a] + w;
+        for (int i = 0; i < k; i++) {
+            System.arraycopy(dist, 0, backup, 0, n + 1);
+            for (int j = 0; j <= n; j++) {
+                for (Node cur = heads[j]; cur != null; cur = cur.next) {
+                    int next = cur.dst;
+                    if (dist[next] > backup[j] + cur.weight) {
+                        dist[next] = backup[j] + cur.weight;
+                    }
                 }
             }
         }
-
-        return dist[dst] > Integer.MAX_VALUE / 4 ? -1 : dist[dst];
+        return dist[dst] >= Integer.MAX_VALUE / 4 ? "impossible" : String.valueOf(dist[dst]);
     }
 
-    public static class Edge {
-        int src;
+    public static int compareTo(int a, int b) {
+        return dist[a] - dist[b];
+    }
+
+    public static void addEdge(int a, int b, int w) {
+        Node node = new Node(b, w);
+        node.next = heads[a];
+        heads[a] = node;
+    }
+
+    public static class Node {
         int dst;
         int weight;
+        Node next;
 
-        public Edge(int src, int dst, int weight) {
-            this.src = src;
+        public Node(int dst, int weight) {
             this.dst = dst;
             this.weight = weight;
         }
