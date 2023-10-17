e2011c8f1384428bd889f6300cbc1d8c401e9dbb
CyS2020 <1440870444@qq.com>
2023-10-17 20:38:37 +0800

    更新

--- src/main/java/leetcode/NextPermutation.java
+++ src/main/java/leetcode/NextPermutation.java
@@ -4,7 +4,7 @@ package leetcode;
  * @author: CyS2020
  * @date: 2021/8/22
  * 描述：下一个排列
- * 思路：从后往前找第一个降序的元素nums[i]，再从后往前找比该元素大的第一个元素nums[j]
+ * 思路：从后往前找第一个降序的前一个元素nums[i]，再从后往前找比该元素大的第一个元素nums[j]
  * 交换两个元素，并且将i后面的元素由降序改为升序即可
  */
 public class NextPermutation {
