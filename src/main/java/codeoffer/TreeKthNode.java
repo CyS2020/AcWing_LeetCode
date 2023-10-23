a9714971291265bcae86fa3fae922fc8a7f01b66
CyS2020 <34827011+CyS2020@users.noreply.github.com>
2023-10-23 11:07:00 +0800

    Create TreeKthNode.java

---
+++ src/main/java/codeoffer/TreeKthNode.java
@@ -0,0 +1,33 @@
+package codeoffer;
+
+import base.TreeNode;
+
+/**
+ * @author: CyS2020
+ * @date: 2021/7/4
+ * 描述：二叉搜索树的第k大节点
+ * 思路：从大到小的中序遍历，如果是第k小的数则就是正常中序遍历
+ */
+public class TreeKthNode {
+
+    public int k, res;
+
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.right);
+        k--;
+        if (k == 0) {
+            res = root.val;
+            return;
+        }
+        dfs(root.left);
+    }
+}
