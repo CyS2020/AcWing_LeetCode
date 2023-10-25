4995ac604b2d6e4e2d2636971549ef9273e616b5
CyS2020 <34827011+CyS2020@users.noreply.github.com>
2023-10-25 10:16:20 +0800

    Create LongestPalindromeSubseq.java

---
+++ src/main/java/leetcode/LongestPalindromeSubseq.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class LongestPalindromeSubseq {
+
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] f = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            f[i][i] = 1;
+            char c1 = s.charAt(i);
+            for (int j = i + 1; j < n; j++) {
+                char c2 = s.charAt(j);
+                if (c1 == c2) {
+                    f[i][j] = f[i + 1][j - 1] + 2;
+                } else {
+                    f[i][j] = Math.max(f[i + 1][j], f[i][j - 1]);
+                }
+            }
+        }
+        return f[0][n - 1];
+    }
+}
